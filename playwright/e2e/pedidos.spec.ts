5c398cf66cacad2011b7568c12a9ee96762697c2
juciellenBM <juciellen.borges@gmail.com>
2026-07-07 21:54:51 -0300

    ajuste codigo para ficar igual do professor

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,28 +1,22 @@
 import { test } from '@playwright/test'
 
-import { HeaderNav } from '../support/components/HeaderNav'
 import { generateOrderCode } from '../support/helpers'
-import { LandingPage } from '../support/pages/LandingPage'
-import { OrderDetails, OrderLockupPage } from '../support/pages/OrderLockupPage'
 
-/// AAA - Arrange, Act, Assert
+import { Navbar } from '../support/components/Navbar'
+
+import { LandingPage } from '../support/pages/LandingPage'
+import { OrderLockupPage, OrderDetails } from '../support/pages/OrderLockupPage'
 
 test.describe('Consulta de Pedido', () => {
 
+  let orderLockupPage: OrderLockupPage
+
   test.beforeEach(async ({ page }) => {
-    const landingPage = new LandingPage(page)
-    const headerNav = new HeaderNav(page)
-    const orderLockupPage = new OrderLockupPage(page)
+    await new LandingPage(page).goto()
+    await new Navbar(page).orderLockupLink()
 
-    // Arrange
-    await landingPage.abrir()
-   
-
-    // Act
-    await headerNav.irParaConsultaPedido()
-
-    // Assert
-    await orderLockupPage.validarPaginaCarregada()
+    orderLockupPage = new OrderLockupPage(page)
+    await orderLockupPage.validateOrderNotFound()
   })
 
   test('deve consultar um pedido aprovado', async ({ page }) => {
@@ -40,21 +34,16 @@ test.describe('Consulta de Pedido', () => {
     payment :'À Vista'
    }
 
-    // Act
-    const orderLockupPage = new OrderLockupPage(page)
-    await orderLockupPage.searchOrder(order.number)
+   await orderLockupPage.searchOrder(order.number)
 
-    // Assert
-    await orderLockupPage.validateOrder(order)
+   await orderLockupPage.validateOrderDetails(order)
+   await orderLockupPage.validateStatusBadge(order.status)
 
   })
 
 
   test('deve consultar um pedido reprovado', async ({ page }) => {
-    // Dado que o usuário está na página de consulta de pedidos
-    //Test Data
-   //const order = 'VLO-EHWTGA'
-
+    // Test Data
    const order: OrderDetails = {
     number: 'VLO-GOUQJH',
     status: 'REPROVADO' as const,
@@ -68,18 +57,16 @@ test.describe('Consulta de Pedido', () => {
     payment :'À Vista'
    }
 
-     // Act
-     const orderLockupPage = new OrderLockupPage(page)
-     await orderLockupPage.searchOrder(order.number)
+   await orderLockupPage.searchOrder(order.number)
 
-     // Assert
-        await orderLockupPage.validateOrder(order)
-      })
+   await orderLockupPage.validateOrderDetails(order)
+   await orderLockupPage.validateStatusBadge(order.status)
+  })
 
 
 
    test('deve consultar um pedido em analise', async ({ page }) => {
-// test data
+  // Test Data
    const order: OrderDetails = {
     number: 'VLO-0YFPJY',
     status: 'EM_ANALISE' as const,
@@ -92,34 +79,25 @@ test.describe('Consulta de Pedido', () => {
     },
     payment :'À Vista'
   }
+  await orderLockupPage.searchOrder(order.number)
 
-   // Act
-   const orderLockupPage = new OrderLockupPage(page)
-   await orderLockupPage.searchOrder(order.number)
-
-   // Assert
-   await orderLockupPage.validateOrder(order)
+  await orderLockupPage.validateOrderDetails(order)
+  await orderLockupPage.validateStatusBadge(order.status)
  })
 
  test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
 
    const order = generateOrderCode()
 
-   const orderLockupPage = new OrderLockupPage(page)
    await orderLockupPage.searchOrder(order)
-
-
-  await orderLockupPage.validateOrderNotFound()
+   await orderLockupPage.validateOrderNotFound()
 
  })
 
- test('deve exibir mensagem quando o pedido hardcoded nao e encontrado', async ({ page }) => {
-  const orderLockupPage = new OrderLockupPage(page)
-  await orderLockupPage.searchOrder('ABC123')
+ test('deve exibir mensagem quando o código do pedido está fora do padrão', async ({ page }) => {
+  const orderCode = 'XYZ-999-INVALIDO'
 
-
- await orderLockupPage.validateOrderNotFound()
-
-})
-
+  await orderLockupPage.searchOrder(orderCode)
+  await orderLockupPage.validateOrderNotFound()
+  })
 })
--- playwright/support/pages/LandingPage.ts
+++ playwright/support/pages/LandingPage.ts
@@ -3,7 +3,7 @@ import { Page, expect } from '@playwright/test'
 export class LandingPage {
     constructor(private page: Page) { }
 
-    async abrir() {
+    async goto() {
         await this.page.goto('/')
         const title = this.page.getByTestId('hero-section').getByRole('heading')
         await expect(title).toContainText('Velô Sprint')
