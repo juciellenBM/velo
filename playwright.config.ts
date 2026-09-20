c1f6bfe97d101f19bcfd9701ab88a8c5d2500f80
juciellenBM <juciellen.borges@gmail.com>
2026-09-20 20:10:24 -0300

    Correção de espaço em branco

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: ' https://velo-da5s499rb-juekarolcontas.vercel.app',
+    baseURL: 'https://velo-da5s499rb-juekarolcontas.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
